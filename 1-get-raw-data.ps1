e5507f3dff7c633072240f723c61f347e8a915c0
Roman Klimenko <romaklimenko@gmail.com>
2015-05-31 10:57:56 +0200

    1. get raw data

---
+++ 1-get-raw-data.ps1
@@ -0,0 +1,4 @@
+﻿for($i = 1; $i -le 12000; $i++) {
+    Write-Host $i
+    Invoke-WebRequest -Uri "http://live.ultimate.dk/desktop/front/data.php?eventid=2601&mode=participantinfo&pid=$i&language=us" -OutFile .\raw-data\$i-raw.html
+}
