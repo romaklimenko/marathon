74a99cea08b479c92b4a6dbbb6e6caeeb4f92b17
Roman Klimenko <romaklimenko@gmail.com>
2015-05-31 18:54:22 +0200

    2. fix raw data

---
+++ 2-fix-raw-data.ps1
@@ -0,0 +1,6 @@
+﻿foreach($raw_file in Get-ChildItem .\raw-data){
+    $content = Get-Content $raw_file.FullName
+    $content = $content[0].Replace("document.getElementById('PARTICIPANTINFO').innerHTML='", "")
+    $content = $content.Substring(0, $content.Length - 2)  
+    $content | Out-File $raw_file.FullName
+}
